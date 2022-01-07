f2da4ccf063807cfe02f022d994868f90e83808b
Jeff Lai <pjefflai@gmail.com>
2022-01-07 09:54:36 -0800

    add all models

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public string Description { get; set; }
+
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
+
+  }
+}
